d92a886dd464d4c269d3fdce95d388aa60fb3cfb
Younes4260 <70180315+Younes4260@users.noreply.github.com>
2021-02-19 23:07:52 -0500

    Presque fini

--- themes/theme-4w4/header.php
+++ themes/theme-4w4/header.php
@@ -44,7 +44,6 @@
 				<p class="site-description"><?php echo $theme_em_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
-
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><img src="https://s2.svgbox.net/hero-solid.svg?ic=menu&color=000000" width="32" height="32"></button>
 			<?php
